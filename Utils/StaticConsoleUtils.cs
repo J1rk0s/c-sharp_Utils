85a5d119b39111b1d97299072ac83a36c22e22f3
J1rk0s <J1rk0s>
2022-05-30 20:02:14 +0200

    Some c# utils

---
+++ Utils/StaticConsoleUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Utils {
+    public static class StaticConsoleUtils {
+        public static string ReverseString(this string txtToReverse) => new string(txtToReverse.Reverse().ToArray());
+
+        public static dynamic Input<T>(string txtToShow)
+        {
+            Console.Write(txtToShow);
+            string inp = Console.ReadLine();
+            
+            if (typeof(T) == typeof(int))
+            {
+                return Convert.ToInt32(inp);
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                return inp;
+            }
+
+            if (typeof(T) == typeof(bool))
+            {
+                return Convert.ToBoolean(inp);
+            }
+
+            return null;
+        }
+
+        public static string Capitalize(this string txt)
+        {
+            if (txt.Length == 1)
+            {
+                return txt.ToUpper();
+            }
+            char[] chars = txt.ToCharArray();
+            string letter = Convert.ToString(txt[0]);
+            letter = letter.ToUpper();
+            chars[0] = Convert.ToChar(letter);
+            return new string(chars);
+        }
+    }
+}
