c78211a2df9e8b8b5d65354311a632db41475bcd
J1rk0s <J1rk0s>
2022-06-08 17:01:42 +0200

    Added more things

--- Utils/StaticConsoleUtils.cs
+++ Utils/StaticConsoleUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Utils {
@@ -27,18 +28,45 @@ namespace Utils {
 
             return null;
         }
+        public static string Capitalize(this string txt) => txt[0].ToString().ToUpper() + txt.Substring(1);
 
-        public static string Capitalize(this string txt)
-        {
-            if (txt.Length == 1)
-            {
-                return txt.ToUpper();
+        public static int QuadraticEquationDiscriminant(int a, int b, int c) => (b * b) - 4 * a * c;
+
+        public static int? BinarySearch(int[] array, int targetNumber) {   // Useless
+            int mid = (array.Length + 1) / 2;
+            int[] first = array.Take(mid).ToArray();
+            int[] second = array.Skip(mid).ToArray();
+
+            if (mid > targetNumber) {
+                foreach (int firstAr in first) {
+                    return Array.IndexOf(first, targetNumber);
+                }
             }
-            char[] chars = txt.ToCharArray();
-            string letter = Convert.ToString(txt[0]);
-            letter = letter.ToUpper();
-            chars[0] = Convert.ToChar(letter);
-            return new string(chars);
+            else if(mid < targetNumber){
+                foreach (int secondAr in second) {
+                    return Array.IndexOf(second, targetNumber);
+                }
+            }
+
+            return null;
+        }
+
+        public static int EuclidAlgo(int a, int b) {
+            while (b > 0) {
+                int c = a / b;
+                int d = a - c * b;
+                a = b;
+                b = d;
+            }
+            return a;
+        }
+
+        public static int MultiplyAllInList(this int[] arr) {
+            int val = 0;
+            foreach (int i in arr) {
+                val *= i;
+            }
+            return val;
         }
     }
 }
