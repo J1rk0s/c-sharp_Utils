6cdf29ebb4ec8259d52110b9b475f0d342279603
J1rk0s <J1rk0s>
2022-06-26 18:14:39 +0200

    Added more functions

--- Utils/UtilsConsole.cs
+++ Utils/UtilsConsole.cs
@@ -16,6 +16,8 @@ namespace Utils {
         }
         public static string ReverseString(this string txtToReverse) => new string(txtToReverse.Reverse().ToArray());
 
+        public static int ReverseInt(this int intToReverse) => Convert.ToInt32(intToReverse.ToString().ReverseString());
+        
         public static dynamic Input<T>(string txtToBeShown, ConsoleColor color = ConsoleColor.White) {
             Console.ForegroundColor = color;
             Console.Write(txtToBeShown);
@@ -65,7 +67,6 @@ namespace Utils {
                     return Array.IndexOf(second, targetNumber);
                 }
             }
-
             return null;
         }
 
@@ -79,13 +80,17 @@ namespace Utils {
             return a;
         }
 
-        public static int MultiplyAllInList(this int[] arr) {
-            int val = 1;
-            foreach (int i in arr) {
-                val *= i;
+        public static int? LinearSearchAlgo(IEnumerable<int> arrayOfNumbers, int targetNumber) {
+            foreach (int numba in arrayOfNumbers) {
+                if (numba == targetNumber) {
+                    return numba;
+                }
             }
-            return val;
+            return null;
         }
+
+        public static int MultiplyAllInList(this IEnumerable<int> arr) => arr.Aggregate((current, i) => current * i);
+
         public static float MultiplyAllInList(this float[] arr) {
             float val = 1;
             foreach (float i in arr) {
@@ -95,7 +100,7 @@ namespace Utils {
         }
 
         public static string[] Similar(this string txt){
-            HashSet<string> similars = new HashSet<string>(){txt.ToLower(), txt.ToUpper(), txt.Capitalize()};
+            HashSet<string> similars = new HashSet<string>{txt.ToLower(), txt.ToUpper(), txt.Capitalize()};
             if (txt.Contains('i') || txt.Contains('I')) {
                 similars.Add(txt.Replace('i', '1'));
                 similars.Add(txt.Replace('I', '1'));
@@ -107,13 +112,45 @@ namespace Utils {
                 similars.Add(txt.Replace('e', '€'));
                 similars.Add(Regex.Replace(txt, @"[Ee]", "€"));
             }
+            if (txt.Contains('O') || txt.Contains('o')) {
+                similars.Add(txt.Replace('O', '0'));
+                similars.Add(txt.Replace('o', '0'));
+                similars.Add(Regex.Replace(txt, @"[Oo]", "0"));
+            }
+            if (txt.Contains('S') || txt.Contains('s')) {
+                similars.Add(txt.Replace('S', '$'));
+                similars.Add(txt.Replace('s', '$'));
+                similars.Add(Regex.Replace(txt, @"[Ss]", "$"));
+            }
+            if (txt.Contains(' ') || txt.Contains(' ')) {
+                similars.Add(txt.Replace(' ', '_'));
+            }
+            
             return similars.ToArray();
         }
 
+        public static string SpongebobCase(string txt) {
+            string[] sr = new string[txt.Length];
+            for (int i = 0; i < txt.Length; i++) {
+                if (i % 2 == 0) {
+                    sr[i] = txt[i].ToString().ToUpper();
+                    continue; 
+                }
+                sr[i] = txt[i].ToString().ToLower();
+            }
+            return string.Concat(sr);
+        }
+
+        public static unsafe IntPtr GetAdressOfIntArray(int[] array) {
+            fixed (int* ptr = array) {
+                return new IntPtr(&ptr);
+            }
+        }
+        
         public static List<T> RemoveDuplicates<T>(this List<T> list) => new HashSet<T>(list).ToList();
         public static T[] RemoveDuplicates<T>(this T[] array) => new HashSet<T>(array).ToArray();
 
-        public static void Predictor(string pathToData) { // Make predictor in other language and pass data to it
+        public static void Predictor(string pathToData) { 
             Console.WriteLine("Undergoing work!!");
         }
     }
